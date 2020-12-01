df6668cfafac641c193f8a6dd1f9b917a8e1a5ee
Jake Stanger <mail@jstanger.dev>
2020-12-01 23:45:02 +0000

    feat(2020): day 1

---
+++ rust/src/year2020/day01.rs
@@ -0,0 +1,33 @@
+use std::io::{stdin, BufRead};
+
+const GOAL: i32 = 99920044;
+
+pub fn run() {
+    let stdin = stdin();
+    let nums: Vec<i32> = stdin
+        .lock()
+        .lines()
+        .filter_map(|line| {
+            let value = line.unwrap().parse().unwrap();
+            if value < GOAL {
+                Some(value)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    for num in &nums {
+        for num2 in &nums {
+            if num + num2 >= GOAL {
+                continue;
+            }
+
+            let partner = nums.iter().find(|&num3| num + num2 + num3 == GOAL);
+            if let Some(num3) = partner {
+                println!("{}", num3 * num * num2);
+                return;
+            }
+        }
+    }
+}
