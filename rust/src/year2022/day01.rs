8b182e4050b173bc3c980ad2f973fb7c56be8c72
Jake Stanger <mail@jstanger.dev>
2022-12-01 20:50:53 +0000

    refactor(year 2022 day 1): more iterators

--- rust/src/year2022/day01.rs
+++ rust/src/year2022/day01.rs
@@ -1,23 +1,17 @@
+use itertools::Itertools;
 use std::cmp::max;
 use std::io::{stdin, BufRead};
-use itertools::Itertools;
 
 pub fn run() {
     let res: u32 = stdin()
         .lock()
         .lines()
         .map(|part| part.unwrap())
-        .enumerate()
-        .fold(vec![vec![]], |mut acc, (i, n)| {
-            if !n.is_empty() {
-                acc.last_mut().unwrap().push(n.parse::<u32>().unwrap());
-            } else {
-                acc.push(vec![]);
-            }
-            acc
-        })
-        .into_iter()
-        .map(|vec| vec.into_iter().sum())
+        .collect::<Vec<String>>()
+        .join("\n")
+        .split("\n\n")
+        .map(|lines| lines.split('\n').map(|n| n.parse::<u32>().unwrap()))
+        .map(|vec| vec.sum())
         .sorted_by(|a: &u32, b: &u32| b.cmp(a))
         .take(3)
         .sum();
