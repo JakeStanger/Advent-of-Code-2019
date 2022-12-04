b63abb06e0114e78d953d5388724647d9b62a4c1
Jake Stanger <mail@jstanger.dev>
2022-12-04 13:41:42 +0000

    refactor(2022): day 4

--- src/year2022/day04.rs
+++ src/year2022/day04.rs
@@ -1,20 +1,17 @@
 use std::io::{stdin, BufRead};
-use std::ops::Range;
 
-fn to_range(str: &str) -> Range<u32> {
-    let (min, max) = str.split_once('-').unwrap();
-    Range {
-        start: min.parse().unwrap(),
-        end: max.parse().unwrap(),
-    }
+fn to_range(str: &str) -> (u32, u32) {
+    str.split_once('-')
+        .map(|(min, max)| (min.parse().unwrap(), max.parse().unwrap()))
+        .unwrap()
 }
 
-fn engulfs(range1: &Range<u32>, range2: &Range<u32>) -> bool {
-    range1.start <= range2.start && range1.end >= range2.end
+fn engulfs((a1, a2): (u32, u32), (b1, b2): (u32, u32)) -> bool {
+    a1 <= b1 && a2 >= b2
 }
 
-fn overlaps(range1: &Range<u32>, range2: &Range<u32>) -> bool {
-    range1.start <= range2.end && range2.start <= range2.end
+fn overlaps((a1, a2): (u32, u32), (b1, b2): (u32, u32)) -> bool {
+    a1 <= b2 && b1 <= a2
 }
 
 pub fn run() {
@@ -26,8 +23,8 @@ pub fn run() {
                 .map(|(a, b)| (to_range(a), to_range(b)))
                 .unwrap()
         })
-        // .filter(|(a, b)| engulfs(&a, &b) || engulfs(&b, &a))
-        .filter(|(a, b)| overlaps(&a, &b) && overlaps(&b, &a))
+        // .filter(|(a, b)| engulfs(*a, *b) || engulfs(*b, *a))
+        .filter(|(a, b)| overlaps(*a, *b) && overlaps(*b, *a))
         .count();
 
     println!("{res:?}");
